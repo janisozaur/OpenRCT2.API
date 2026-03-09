bc93cb6dd1e2b2dcd9766ff5307af3389ff19593
Michał Janiszewski <janisozaur@gmail.com>
2026-03-09 19:13:55 +0100

    Fix deprecations in .NET 10

--- test/ApiIntegrationTests/TestLocalisation.cs
+++ test/ApiIntegrationTests/TestLocalisation.cs
@@ -2,24 +2,39 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
 using OpenRCT2.API;
 using Xunit;
 
 namespace ApiIntegrationTests
 {
-    public class TestLocalisation
+    public class TestLocalisation : System.IDisposable
     {
+        private readonly IHost _host;
         private readonly TestServer _server;
         private readonly HttpClient _client;
 
         public TestLocalisation()
         {
-            var webHostBuilder = new WebHostBuilder()
-                .UseEnvironment(Environments.Testing)
-                .UseStartup<Startup>();
+            _host = new HostBuilder()
+                .ConfigureWebHost(webBuilder =>
+                {
+                    webBuilder
+                        .UseEnvironment(OpenRCT2.API.Environments.Testing)
+                        .UseStartup<Startup>()
+                        .UseTestServer();
+                })
+                .Start();
 
-            _server = new TestServer(webHostBuilder);
+            _server = _host.GetTestServer();
             _client = _server.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+            _server.Dispose();
+            _host.Dispose();
         }
 
         [Theory(Skip = "progressed.io is down")]
